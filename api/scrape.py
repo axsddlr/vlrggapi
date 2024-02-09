243f982e9d4172c1d4a0ac00b9e8714862046997
Andre Saddler <dresadd09691@gmail.com>
2024-02-09 09:56:04 -0500

    scrape.py: update user-agent

--- api/scrape.py
+++ api/scrape.py
@@ -10,8 +10,7 @@ from utils.utils import headers
 class Vlr:
     def __init__(self):
         self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/74.0.3729.169 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0",
         }
 
     def get_parse(self, url):
