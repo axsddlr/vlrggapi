7616430f16b7457d305312548c172ed2c9e9011c
Diego <didipinheiro@outlook.com>
2025-08-13 23:10:24 -0300

    feat(api): enhance vlr_events function to include prize pool and event dates

--- api/scrapers/events.py
+++ api/scrapers/events.py
@@ -13,19 +13,36 @@ def vlr_events(region_key):
 
 
     for event in html.css("a.wf-card"):
-       
       item = event.css_first("div.event-item-inner")
       eventName = item.css_first("div.event-item-title").text().strip()
       eventStatus = item.css_first("div.event-item-desc-item").css_first("span").text().strip()
       logo = event.css_first("div.event-item-thumb").css_first("img").attributes["src"]
-
+      prizePool = item.css_first("div.mod-prize").text(strip=True, deep=False)
       logo = re.sub(r"\/img\/vlr\/tmp\/vlr.png", "", logo)
+      dates = item.css_first("div.mod-dates").text(strip=True, deep=False)
+
+
+
+      if "—" in dates:
+        date_from = dates.split("—")[0].strip()
+        date_to = dates.split("—")[1].strip() 
+      else:
+        date_from = dates.strip()
+        date_to = dates.strip()
+
+      if " " not in date_to:
+        month = date_from.split(" ")[0]
+        date_to = f"{month} {date_to}"
+
 
       result.append(
         {
            "eventName": eventName,
            "eventStatus": eventStatus,
-           "logo": logo or None
+           "prizePool": prizePool,
+           "logo": logo,
+           "date-from": date_from,
+           "date-to": date_to
         }
       )
 
