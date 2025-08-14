cdfcf3402242de13033f0a7f38371ab063de95d1
Diego <didipinheiro@outlook.com>
2025-08-14 00:20:17 -0300

    feat(api): add month mapping for improved date handling

--- utils/utils.py
+++ utils/utils.py
@@ -19,3 +19,18 @@ region = {
     "jp": "japan",
     "col": "collegiate",
 }
+
+month = {
+  "jan": "January",
+  "feb": "February",
+  "mar": "March",
+  "apr": "April",
+  "may": "May",
+  "jun": "June",
+  "jul": "July",
+  "aug": "August",
+  "sep": "September",
+  "oct": "October",
+  "nov": "November",
+  "dec": "December"
+}
