feda14f2378db4465db757dd2dcf3aef01b5805a
Chris Lim <22702896+chrisdlim@users.noreply.github.com>
2026-01-06 11:48:21 -0500

    Add extended version for fetching upcoming matches

--- api/scrapers/__init__.py
+++ api/scrapers/__init__.py
@@ -1,6 +1,6 @@
 from .news import vlr_news
 from .rankings import vlr_rankings
 from .stats import vlr_stats
-from .matches import vlr_upcoming_matches, vlr_live_score, vlr_match_results
+from .matches import vlr_upcoming_matches, vlr_live_score, vlr_match_results, vlr_upcoming_matches_extended
 from .events import vlr_events
 from .health import check_health
